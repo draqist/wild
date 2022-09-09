5ea13ef69e4294ae0b6d1a9ccad61b070457e6a7
Draqode <Boomjay008@gmail.com>
2022-09-09 03:55:01 +0100

    finished the whole assesment and everywhere looks green

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,30 +3,6 @@ import { carouseldata } from "./data";
 
 const { innerWidth } = window;
 const length = carouseldata.length;
-export const handlePrevScroll = (
-  nextslide: number,
-  prevslide: number,
-  setNextSlider: Dispatch<SetStateAction<number>>,
-  setPrevSlider: Dispatch<SetStateAction<number>>
-) => {
-  const flowers = document.getElementById("4");
-  const element = document.getElementById("wrapper");
-  if (nextslide === 1 && prevslide === 4) {
-    flowers?.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-    setPrevSlider(prevslide === 4 ? prevslide - 1 : prevslide - 1);
-    setNextSlider(prevslide === length - 1 ? 0 : prevslide + 1);
-  } else {
-    setPrevSlider(prevslide === 0 ? 4 : prevslide - 1);
-    setNextSlider(prevslide === length - 1 ? 0 : prevslide + 1);
-    element?.scrollBy({
-      left: -innerWidth,
-      behavior: "smooth",
-    });
-  }
-};
 export const handleNextScroll = (
   nextslide: number,
   prevslide: number,
@@ -47,6 +23,30 @@ export const handleNextScroll = (
     setPrevSlider(nextslide === 0 ? 4 : nextslide - 1);
     element?.scrollBy({
       left: innerWidth,
+      behavior: "smooth",
+    });
+  }
+};
+export const handlePrevScroll = (
+  nextslide: number,
+  prevslide: number,
+  setNextSlider: Dispatch<SetStateAction<number>>,
+  setPrevSlider: Dispatch<SetStateAction<number>>
+) => {
+  const flowers = document.getElementById("4");
+  const element = document.getElementById("wrapper");
+  if (nextslide === 1 && prevslide === 4) {
+    flowers?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+    setPrevSlider(prevslide === 4 ? prevslide - 1 : prevslide - 1);
+    setNextSlider(prevslide === length - 1 ? 0 : prevslide + 1);
+  } else {
+    setPrevSlider(prevslide === 0 ? 4 : prevslide - 1);
+    setNextSlider(prevslide === length - 1 ? 0 : prevslide + 1);
+    element?.scrollBy({
+      left: -innerWidth,
       behavior: "smooth",
     });
   }
